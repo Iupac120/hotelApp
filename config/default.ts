fccd52fe2f3cc41db77e36e1b12f2ba8cb69689f
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2023-09-11 12:39:46 +0100

    updated script

--- config/default.ts
+++ config/default.ts
@@ -27,5 +27,5 @@ export default {
     user:process.env.NODEMAILER_USER,
     pass:process.env.NODEMAILER_PASS,
     emailedFrom:process.env.EmailedFrom,    
-    stripe:process.env.STRIPE
+    //stripe:process.env.STRIPE
 };
